a912f9e9526a310c3c6debb064805aeb80a9d6b3
maha1630 <78270519+maha1630@users.noreply.github.com>
2021-01-31 10:53:38 +0000

    Add files via upload

---
+++ skincareQuiz.js
@@ -0,0 +1,84 @@
+//Global Variables:
+//This is where you will define the variables you will be using in your project.
+//#TODO: Create four variables to track each possible quiz outcome
+var questionCount = 0;
+var oilySkinScore = 0;
+var normalSkinScore = 0;
+var combinationSkinScore = 0;
+var drySkinScore = 0;
+
+var q1a1 = 0;
+var q1a2 = 0;
+var q1a3 = 0;
+var q1a4 = 0;
+var q2a1 = 0;
+var q2a2 = 0;
+var q2a3 = 0;
+var q2a4 = 0;
+var q3a1 = 0;
+var q3a2 = 0;
+var q3a3 = 0;
+var q3a4 = 0;
+var q4a1 = 0;
+var q4a2 = 0;
+var q4a3 = 0;
+var q4a4 = 0;
+var q5a1 = 0;
+var q5a2 = 0;
+var q5a3 = 0;
+var q5a4 = 0;
+var q6a1 = 0;
+var q6a2 = 0;
+var q6a3 = 0;
+var q6a4 = 0;
+
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
+var q6a1 = document.getElementById("q6a1");
+var q6a2 = document.getElementById("q6a2");
+var q6a3 = document.getElementById("q6a3");
+var q6a4 = document.getElementById("q6a4");
+
+q1a1.addEventListener("click", oilySkinScore);
+q1a2.addEventListener("click", normalSkinScore);
+q1a3.addEventListener("click", combinationSkinScore);
+q1a4.addEventListener("click", drySkinScore);
+q2a1.addEventListener("click", oilySkinScore);
+q2a2.addEventListener("click", normalSkinScore);
+q2a3.addEventListener("click", combinationSkinScore);
+q2a4.addEventListener("click", drySkinScore);
+q3a1.addEventListener("click", oilySkinScore);
+q3a2.addEventListener("click", normalSkinScore);
+q3a3.addEventListener("click", combinationSkinScore);
+q3a4.addEventListener("click", drySkinScore);
+q4a1.addEventListener("click", oilySkinScore);
+q4a2.addEventListener("click", normalSkinScore);
+q4a3.addEventListener("click", combinationSkinScore);
+q4a4.addEventListener("click", drySkinScore);
+q5a1.addEventListener("click", oilySkinScore);
+q5a2.addEventListener("click", normalSkinScore);
+q5a3.addEventListener("click", combinationSkinScore);
+q5a4.addEventListener("click", drySkinScore);
+q6a1.addEventListener("click", oilySkinScore);
+q6a2.addEventListener("click", normalSkinScore);
+q6a3.addEventListener("click", combinationSkinScore);
+q6a4.addEventListener("click", drySkinScore);
+
